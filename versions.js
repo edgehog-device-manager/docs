b583379d7e15880101f688e6b5194b5ab02d0352
Luca Zaninotto <luca.zaninotto@secomind.com>
2026-04-21 17:09:20 +0200

    chore: remove wrongful docs entry (#7)
    - Remove wrongful docs entry: the docs entry was wrongfully created due to a workflow running when not needed.
    - Update versions: a wrongful run resetted the `versions.js` file
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- versions.js
+++ versions.js
@@ -1,22 +1,34 @@
 var versionNodes = [
-  {
-    version: "v0.11.0",
-    url: "https://docs.edgehog.io/0.11"
-  },
-  {
-    version: "v0.10.0",
-    url: "https://docs.edgehog.io/0.10"
-  },
-  {
-    version: "v0.9.3",
-    url: "https://docs.edgehog.io/0.9"
-  },
-  {
-    version: "v0.8.0",
-    url: "https://docs.edgehog.io/0.8"
-  },
-  {
-    version: "v0.7.1",
-    url: "https://docs.edgehog.io/0.7"
-  }
+    {
+        version: "snapshot (unreleased)",
+        url: "https://docs.edgehog.io/snapshot",
+    },
+    {
+        version: "v0.12",
+        url: "https://docs.edgehog.io/0.12",
+    },
+    {
+        version: "v0.11",
+        url: "https://docs.edgehog.io/0.11",
+    },
+    {
+        version: "v0.10",
+        url: "https://docs.edgehog.io/0.10",
+    },
+    {
+        version: "v0.9",
+        url: "https://docs.edgehog.io/0.9",
+    },
+    {
+        version: "v0.8",
+        url: "https://docs.edgehog.io/0.8",
+    },
+    {
+        version: "v0.7",
+        url: "https://docs.edgehog.io/0.7",
+    },
+    {
+        version: "v0.5",
+        url: "https://docs.edgehog.io/0.5",
+    },
 ]
